99bc8d801239cc398adba38a220608cc4ce13c70
devtooligan <devtooligan@tooligan.dev>
2023-09-28 12:43:01 -0700

    fix

--- src/utils/CREATOR.sol
+++ src/utils/CREATOR.sol
@@ -10,7 +10,7 @@ library CREATOR {
     // IMPORTANT: CREATOR TO UPDATE THIS!
     // This function returns true or false if the solution is correct.
     // Write logic that will check if the provided solution is correct
-    function verify(address solution) public returns (bool) {
+    function verify(address solution) internal returns (bool) {
         // add logic to verify solution
         return false;
     }
@@ -18,7 +18,7 @@ library CREATOR {
     // IMPORTANT: CREATOR TO UPDATE THIS!
     // his function should return the number that should reported for gas for the solution.
     // It could be the measurement of a single function call or multiple.
-    function gasReport(address solution)  public returns (uint256 gasUsed) {
+    function gasReport(address solution)  internal returns (uint256 gasUsed) {
         // add logic to report gas cost of the relevant call or calls to the solution
 
         // Example:
@@ -31,7 +31,7 @@ library CREATOR {
 
     }
 
-    function challengeId() public pure returns (uint8) {
+    function challengeId() internal pure returns (uint8) {
         require(_challengeId != 0xff, "IMPORTANT: CREATOR to update challengeId!");
         return _challengeId;
     }
